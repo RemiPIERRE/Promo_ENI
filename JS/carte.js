02944315adca7b3d92f56d86fa3da67c4e22cbfe
Rémi PIERRE <remi.pierre2026@campus-eni.fr>
2026-05-26 11:30:19 +0200

    ADD : Page Carte + Map interactive

---
+++ JS/carte.js
@@ -0,0 +1,24 @@
+let theme = localStorage.getItem('theme') || 'clair';
+document.body.setAttribute('data-theme', theme);
+
+const map = L.map('map').setView([46.603354, 1.888334], 5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+console.log(document.getElementById('map'));
+console.log(document.getElementById('map').offsetHeight);
+
+fetch('assets/data/data.json')
+    .then(response => response.json())
+    .then(data => {
+        data.apprenants.forEach(apprenant => {
+            if (apprenant.coordonnees.latitude !== 0) {
+                L.marker([apprenant.coordonnees.latitude, apprenant.coordonnees.longitude])
+                    .addTo(map)
+                    .bindPopup(`<b>${apprenant.prenom} ${apprenant.nom}</b><br>${apprenant.ville}`);
+            }
+        });
+    });
